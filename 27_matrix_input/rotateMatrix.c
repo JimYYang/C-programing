1c163b7b77a66cce46e8b9aa7bf325d1afe77ba8
Coursera Learner <nobody@nowhere.nul>
2022-11-07 12:00:11 +0000

    27

--- 27_matrix_input/rotateMatrix.c
+++ 27_matrix_input/rotateMatrix.c
@@ -1,108 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <ctype.h>
+#include <errno.h>
 #include <string.h>
 
-void rotate(char matrix[10][10])
-{
-    int N = 10;
-    for (int layer = 0; layer < N / 2; ++layer)
-    {
-        int maxValue = N-layer-1;
-        int minValue = layer;
-        for(int i = minValue; i < maxValue; ++i)
-        {
-        int offset = i - minValue;
-        //temporary top value
-        char top = matrix[minValue][minValue + offset];
-        //top = left
-        matrix[minValue][minValue+offset] = matrix[maxValue - offset][minValue];
-        //left = bottom
-        matrix[maxValue-offset][minValue] = matrix[maxValue][maxValue-offset];
-        //bottom = right
-        matrix[maxValue][maxValue - offset] = matrix[minValue + offset][maxValue];
-        //right = top
-        matrix[minValue + offset][maxValue] = top;
 
+
+void rotate(char m[10][10]) {
+    int n = 10;
+    for (int i = 0; i < n / 2; i ++) {
+        for (int j = 0; j < (n + 1) / 2; j ++) {
+            char t = m[i][j];
+            m[i][j] = m[n - j - 1][i];
+            m[n - j - 1][i] = m[n - i - 1][n - j - 1];
+            m[n - i - 1][n - j - 1] = m[j][n - i - 1];
+            m[j][n - i - 1] = t;
         }
-	}
+    }
 }
 
-int checkInputFormat(char * a)
-{
+
+int CheckInputFormat(char * a) {
     char * suffix = ".txt";
-    size_t slen = strlen(a);
-    size_t suffix_len = strlen(suffix);
-    return suffix_len <= slen && !strcmp(a + slen - suffix_len, suffix);
+    size_t s1 = strlen(a);
+    size_t s2 = strlen(suffix);
+    return s1 >= s2 && (strcmp(a + s1 -s2, suffix) == 0);
 }
-int checkFileContents(FILE * f)
-{
+
+
+int CheckFileContents(FILE * f) {
     int c;
     int rows = 0;
-    int chars = 0;
+    int total_chars = 0;
+
     c = fgetc(f);
-    while(c != EOF)
-    {
-        //if(c == 0xFF) continue;
-        if(c != '\n' && isprint(c)) ++chars;
-        if(c == '\n') ++rows;
+
+    while (c != EOF) {
+        if (c == 0xFF) continue;
+        if (c != '\n' && isprint(c)) ++ total_chars;
+        if (c == '\n') ++ rows;
         c = fgetc(f);
     }
-    if(rows == 10 && chars ==100) return 1;
+    if (rows == 10 && total_chars == 100)
+        return 1;
     return 0;
 }
 
-int main(int argc, char * argv[])
-{
-    if(argc != 2)
-    {
-        perror("Wrong input");;
+
+int main(int argc, char ** argv) {
+    if (argc != 2) {
+        fprintf(stderr,"Usage: encrypt key inputFileName\n");
         return EXIT_FAILURE;
     }
-    if(!checkInputFormat(argv[1]))
-    {
-        perror("Wrong file format");
-        return EXIT_FAILURE;
-    }
+
     FILE * f = fopen(argv[1], "r");
-    if(f == 0)
-    {
-        perror( "Invalid file");
+    if (f == NULL) {
+        perror("Could not open file");
         return EXIT_FAILURE;
     }
-    if(!checkFileContents(f))
-    {
-        perror("File contents invalid");
+
+    char * filename = argv[1];
+
+    if (!CheckInputFormat(filename)) {
+        perror("Invalid file format!");
         return EXIT_FAILURE;
     }
-    rewind(f);
-    char arr[10][10];
+
+    if (!CheckFileContents(f)) {
+        perror("Invalid file contents!");
+        return EXIT_FAILURE;
+    }
+
+    char m[10][10];
+
     int c;
-    for(int i = 0 ; i < 10; ++i)
-    {
-        for(int j = 0; j < 10; ++j)
-        {
-            c = fgetc(f);
-            if(c == '\n') c = fgetc(f);
-            arr[i][j] = c;
+    int row = 0, col = 0;
+    while ((c = fgetc(f)) != EOF) {
+        if (c != '\n') {
+            m[row][col] = c;
+            ++ col;
+            if (col > 9) {
+                ++ row;
+                col = 0;
+            }
         }
-        c = fgetc(f);
     }
-    rotate(arr);
-    for(int a = 0; a < 10; ++a)
-    {
-        for(int b = 0; b < 10; ++b)
-        {
-            printf("%c", arr[a][b]);
+
+    rotate(m);
+
+    for (int i = 0; i < 10; i ++) {
+        for (int j = 0; j < 10; j ++) {
+            printf("%c", m[i][j]);
         }
         printf("\n");
     }
-    if(fclose(f) != 0)
-    {
-        perror("File did not close");
+    
+    if (fclose(f) != 0) {
+        perror("File did not close successfully!");
         return EXIT_FAILURE;
     }
-    return EXIT_SUCCESS;
+
+    return 0;
 }
 
