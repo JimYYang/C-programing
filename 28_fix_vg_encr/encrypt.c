27c306209b8fc87910422820d35e2393f82bf4cf
Coursera Learner <nobody@nowhere.nul>
2022-11-08 12:19:36 +0000

    Did assignment 28

--- 28_fix_vg_encr/encrypt.c
+++ 28_fix_vg_encr/encrypt.c
@@ -1,27 +1,29 @@
+#define  _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
 void encrypt(FILE * f, int key, FILE * outfile){
-  char * line;
-  size_t sz;
-  while (getline(&line,&sz, f) >= 0) {
+  char * line = NULL;
+  size_t sz = 0;
+  while (getline(&line, &sz, f) >= 0) {
     char * ptr = line;
     while (*ptr != '\0') {
       int c = *ptr;
       if (isalpha(c)) {
-	c = tolower(c);
-	c -= 'a';
-	c += key;
-	c %= 26;
-	c += 'a';
+        c = tolower(c);
+        c -= 'a';
+        c += key;
+        c %= 26;
+        c += 'a';
       }
       *ptr = c;
       ptr++;
     }
     fprintf(outfile, "%s", line);
   }
+  free(line); // because of getline will call realloc which will free the before line.
 }
 
 int main(int argc, char ** argv) {
@@ -40,11 +42,12 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
   //outfileNAme is argv[2] + ".txt", so add 4 to its length.
-  char * outFileName = malloc((strlen(argv[2]) + 4) * sizeof(*outFileName));
+  char * outFileName = malloc((strlen(argv[2]) + 5) * sizeof(*outFileName));
   strcpy(outFileName, argv[2]);
   strcat(outFileName, ".enc");
   FILE * outFile = fopen(outFileName, "w");
   encrypt(f,key, outFile);
+  free(outFileName);
   if (fclose(outFile) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
@@ -56,3 +59,4 @@ int main(int argc, char ** argv) {
 
   return EXIT_SUCCESS;
 }
+
