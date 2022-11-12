07e80e795bc008a50ecee3f75dc06a7a20a0d245
Coursera Learner <nobody@nowhere.nul>
2022-11-12 15:04:30 +0000

    c4prj2

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -6,6 +6,7 @@
 deck_t * hand_from_string(const char * str, future_cards_t * fc) {
     deck_t * d = malloc(sizeof(*d));
     d -> cards = NULL;
+    d -> n_cards = 0;
     size_t len_str = strlen(str);
     
     for (size_t i = 0; i < len_str; i ++) {
@@ -17,12 +18,17 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc) {
             while (str[i]!= '\n' && str[i] != ' ') {
                 index[j ++] = str[i ++];
             }
+            index[j] = '\0';
             add_future_card(fc, atoi(index), add_empty_card(d));
         }
         else {
             add_card_to(d, card_from_letters(str[i], str[i + 1]));
             ++i;
         }
+    }
+    if (d -> n_cards < 5) {
+        fprintf(stderr, "Too few cards");
+        return NULL;
     }
     return d;
 }
