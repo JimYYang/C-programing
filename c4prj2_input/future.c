fa9895ec6a270cd2b3114cb19131b45de5990976
Coursera Learner <nobody@nowhere.nul>
2022-11-13 03:37:48 +0000

    change future.c

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -16,7 +16,7 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
             fc -> decks[fc -> n_decks].n_cards = 0;
             ++ fc -> n_decks;
         }
-        fc -> decks[index].cards = realloc(fc -> decks[index].cards, (fc -> n_decks + 1) * sizeof(card_t *));
+        fc -> decks[index].cards = realloc(fc -> decks[index].cards, (fc -> decks[index].n_cards + 1) * sizeof(card_t *));
         fc -> decks[index].cards[fc -> decks[index].n_cards] = ptr;
         ++ fc ->decks[index].n_cards;
     }
